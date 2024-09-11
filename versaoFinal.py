f06413023d3a579bc786f82861ab2e23fcd95674
Miguel Rocha <migsouzarocha@gmail.com>
2024-09-11 00:00:18 -0300

    codigo-final

---
+++ versaoFinal.py
@@ -0,0 +1,38 @@
+pt = '/01NOSSO POVO01/ /02Aqui começa a história do tempo dos antigos02/ /03O início da história03/ /04Antes do mundo existir, Ngutapa já existia.04/ Ele não teve pai nem mãe. Mapana, a mulher de Ngutapa, se criou junto com ele. No mesmo lugar viviam também Baia e sua mulher. /05Baia era parente de Ngutapa.05/ No lugar onde esses quatro se criaram é onde ficava a montanha Taiwegüne, /06É no igarapé Tonetü.06//07 Naquele tempo, a terra ainda estava se formando. O mato era baixinho e o rio ainda tinha pouca água, lá eles viviam.07/ /08Passaram-se muitos anos.08/ Ngutapa e Mapana nunca habitaram juntos. /09Nunca tiveram filhos.09/ Um dia, /10quando o mato já estava crescido,10//11 Ngutapa foi caçar com Mapana.11/ No caminho eles começaram a brigar. /12Ngutapa agarrou sua mulher e lhe deu uma surra.12/ /13Depois disso, amarrou Mapana num pau, de braços e pernas abertos.13/ /14Deixou ela ali e seguiu para caçar no mato.14/ /15Vieram as cabas e as formigas e morderam a sua periquita.15/ /16Ela sofreu muito.16/ /17Então apareceu o pássaro cancã e se sentou no alto do pau onde ela estava amarrada.17/ /19Mapana disse para o cancã:19/ -/20Vovó, pode me desamarrar?20/ /21O pássaro gritou: -Co, co, co, cou!21/ -/22Vovó, venha me desamarrar.22//23 Aquele desgraçado me prendeu aqui para me matar - ela falou de novo.23/ /24O cancã se transformou em gente24/ e, /25chegando mais perto, perguntou25/: -/26O que lhe aconteceu, minha neta?26//27 Se você quiser se vingar de Ngutapa, está aqui a caba27/. /28Ela pegou a caba e guardou28/. A casa de caba era muito grande, mas parecia pequena. O Cancã falou ainda: -Você não pode ficar aí. Tem que esperar o seu marido num lugar onde ele não possa ver. /29Depois disso, o cancã se transformou em pássaro e foi embora29/. Demorou um pouco e /30Ngutapa voltou da caçada30/. /40Vinha tocando flauta e pulava numa perna e noutra, cantando40/: -Por onde anda Mapana?! As cabas e as formigas morderam a periquita dela! /41Tcheruru tcheruru-u-u-u... Tcheruru tcheruru-u-u-u... - assim dizia. 41/ /31Mapana estava31/ /42escondida num tronco de árvore42/, /31esperando Ngutapa passar31/. Escutou esse canto e se preparou. Quando ele chegou, ela jogou a casa de caba em cima dele e /32acertou-lhe os dois joelhos32/. /33Ngutapa caiu e não se levantou mais33/. /34Mapana deixou ele ali mesmo e foi embora34/. /35Ngutapa foi se arrastando até em casa35/. Desde que as cabas ferraram, /36seus joelhos começaram a inchar36/.'
+tc = "/01TORÜ DUǕ'ǕGÜ01/ /02Nori arü ügü ga ore ga nucüma'ǖ ga torü ga na nhunhaacü yi'i'ǖ.02/ /03Norü ügü tchiga03/ /04Nüma ga Ngutapa ga naãne nama'ã ya ĩĩtchicü,04/ rü nge'eǖ rü na tauma i ta'acü i nhũma na yema i na i aẽǖrü'ǖ. Rü yiĩ'ǖ ngema na yamareǖ ga Ngutapa. I nori'ǖtchicü, ingeguma, rü norü tchiura ta ngemana yaǖ, rü ya Baia rü ta cünanangemaǖ rü norü tchiura rü ta nangemaǖ.  Erü nüma ga /05Baia rü Ngutapa tanü'ǖ ni'ĩ rü wü'iva na yae.05/ Nucüma ngeguma rü numa ga Ngutapa rü na tauǖma ga norü bue nu'ǖ tayema yerü tauguma nügüga nidau,  ga namamaã rü na yaemare. Ngeguma yema namamare i ya'ĩ,  rü tauguma i buǖã,  ga ngĩma ga Mapana,  rü yemaca ngitchi naai ga nüma ga Ngutapa. /07Inacü i ngeguma i ngema.  I Taiwegüne i nori naãene i üǖwacü rü ngema ni'ĩ i nangemagüǖ erü ngema i nori naü'ǖ i naãene nua07/ /06natü i Tonetü06/. Erü yeguma ãrü naãenecü rü wü'i i ngewaca i ya eaneǖrǖ'ǖ ni'ĩ yerü natauma ga nhãtü rü yema nawa nayemagüǖ ricatama ni'ĩ. Rü yeguma ngemamã ni'ĩ i nangemagü'ǖ,  /08rü marü nhurema ya taunecǖ na yemagü08/ rü yegumama ni'ĩ ga tüǖ nacuaiǖ ga namã ga Ngutapa rü yeguma rü na tauma ga to,  ga duǖ'ǖ nüĩcagümare ni'ĩ ga nayemagü'ǖ. Natürü ga guma Baia rü nü'ǖ tayema ga bue rü naaãcü nǖetcha nüma ga guma. Ngutapa /09rü tauguma naãcü09/ /50rü mu'utchicüma ya tauenecü na ngeacü50/. /11Rü yeguma fenewa tümamãã na u'ǖ ga namá ga Ngutapa11/ /10yerü yeguma rü marü nayaanegü ga naĩnecü10/, /12rü yema i na feneeügu ni'ĩ ga tü'ǖ na cuai'ǖ ga nama12/,  /13rü yeguma tü'ǖ ya naĩ'ü rü nainewa  tü'ǖ ni'ĩ nai rü ngiparawa ruta, tü'ǖ ni'ĩ naĩgü, rü ngĩtchacü'ǖwa ruta13/. /14Rü yemawena rü nüma ga Ngutapa rü na üpetü ga na ya feneǖ14/. /16Yerü yegumarü marü auriǖtchima nguǖ tinge16/, /15rü maẽ rü tü'ǖ nawi rü ẽne, rü tü'ü na ngo ga tümaãrü ngaǖwa15/. /17Rü Yeguma tümaetügu i na rü wagügü i coou rü ngeguma ngina tacagü17/ /19rü nhãtagürü19/, /20pa noẽ  pa coou tautchiname'ǖ ega tchoǖ icuwẽgǖgu20/ /21nhãtagürü co-co-co-cou nhatagüǖ21/, /22pa noẽ  tauchina meǖ ega tchoǖ icuiwẽgügu22/ /23rü tchoǖ ni'ĩ ma i ucaǖǖtchi i Ngutapa nhãtagürü23/. /25Rü yeguma ga noẽ i coou ta ngica itarü ĩ  rü ngiǖ ta ngaǖ25/ /26rü cu tücü,  pa tchauta'ã nhãtagürü26/. /24Rü yeguma ga noẽ ga Co, co, co, cou rü ĩrü duǖ24/. Rü yeguma rü tü'ǖ ĩ ya wẽgü /27rü ngeguma cuütanütchaǖgu rü daani'ĩ ya maẽ nhanagürü27/, /28rü yeguma rü inayau ga maẽ ga Mapana28/. Natürü ga guma maẽ rü naega rü ngerata ni'ĩ ga naẽga, natütü guma maẽ  rü na taütchirema rü natürü yiema i daugu rü na i'ra.  Rü yeguma i nayau ga maẽ, rü ngigürügü taũǖ tama ni'ĩ nua.  cuǖna ngẽẽǖ ngigürügü. /29Rü marü yemawena ga coou rü tüna i ĩgü rü weriǖ ti'igüãrü29/. Rü yeguma ga nüma ga /30Ngutapa rü marü na taegutchaǖ30/, rü yeguma marü nataegugu rü wowerugu ni'ĩ fetchigü rü yeguma ga /31ngima ga Mapana rü marü iya nguẽẽ31/ /42ga namacüwawa ga naĩpünewa42/. /40Rü nüma ga Ngutapa rü ni'ĩ woweruetchigü rü ni'ĩ wü'i paratchigü, rü ni'ĩ fenagütchigü40/, rü nhãnagürü, nhũmarütchi nhũnhã i Mapana i mae ngi'ǖ wiarü ngaǖãcü rü ẽne ngi'ǖ ngo arü nga'uãcü nhãnagürü!  /41Tcherutcheru-u-u-u... tcherurutcheruru-u-u-u... nhanagürü.41/ Rü yeguma ga tüma ga Mapana rü marü nüǖ ta inü'ǖ rü wüiparagu ni'ĩ ga numa, rü yeguma marü na ngupetüatchigu rü nügü i'ya caiãtchigu, rü yeguma naweama nenanhãga guma maẽ, /32rü pe ga naã pü'ügu rü norü naiãpüüguta32/. /33Rü ngǖ ga ini'ĩ ni'ĩngu, rü marü tama yema i natchi33/ /34rü tüma ga norü na'ma ga Mapana, rü tüma i ti'u rü yema yirüma ni'ĩ rü nüna iti'ãma ga ĩpatawa34/. Rü yeguma ga nüma rü marü taguma i natchi rü nüma ga /36naãpüǖ rü marü manarütchaapüǖ36/, /35rü yemaãcü tama ini'ĩu rü ini waatchigü, rü nhumata inangu ga ĩpatawa ga nayaanegü35/."
+
+listaErrosMarcação = [18,37,38,39]
+dicionarios = {}
+for i in range(1,43):
+    if i in listaErrosMarcação :
+        continue
+    if i < 10:
+        inicioTrechopt = pt.find('/0{}'.format(i))
+        fimTrechopt = pt.find('0{}/'.format(i))
+        dicionarios['trecho{}'.format(i)] = {}
+        dicionarios['trecho{}'.format(i)]['id'] = i-1#adiciona um id ao dicionario
+        dicionarios['trecho{}'.format(i)]['pt'] = pt[inicioTrechopt+3:fimTrechopt]#adiciona os trechos baseados no fatiamento para a chave 'pt'
+        inicioTrechotc = tc.find('/0{}'.format(i))
+        fimTrechotc = tc.find('0{}/'.format(i))
+        dicionarios['trecho{}'.format(i)]['tca'] = tc[inicioTrechotc+3:fimTrechotc]
+    else:
+        inicioTrechopt = pt.find('/{}'.format(i))
+        fimTrechopt = pt.find('{}/'.format(i))
+        dicionarios['trecho{}'.format(i)] = {}
+        dicionarios['trecho{}'.format(i)]['id'] = i-1
+        dicionarios['trecho{}'.format(i)]['pt'] = pt[inicioTrechopt+3:fimTrechopt]
+        inicioTrechotc = tc.find('/{}'.format(i))
+        fimTrechotc = tc.find('{}/'.format(i))
+        dicionarios['trecho{}'.format(i)]['tca'] = tc[inicioTrechotc+3:fimTrechotc]
+print('[')
+for i in range(1,43):
+    if i in listaErrosMarcação:
+        continue
+    print(' {')
+    print('     "id": {},'.format(dicionarios['trecho{}'.format(i)]['id']))
+    print('     "translation": {')
+    print('         "pt": "{}"'.format(dicionarios["trecho{}".format(i)]['pt']))
+    print('         "tca": "{}"'.format(dicionarios["trecho{}".format(i)]['tca']))
+    print('     }')
+    print(' },')
+print(']')
